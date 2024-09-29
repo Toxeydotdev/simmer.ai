565c1b94aa3aa0a8f1e8b3a0e32e051273a3f445
Anthony Tran <atran5591@gmail.com>
2024-09-29 16:02:48 -0700

    typo

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  supabaseUrl: import.meta.env['NG_APP_SUPERBASEURL'] || '',
+  supabaseUrl: import.meta.env['NG_APP_SUPABASEURL'] || '',
   supabaseKey: import.meta.env['NG_APP_SUPABASEKEY'] || '',
 };
