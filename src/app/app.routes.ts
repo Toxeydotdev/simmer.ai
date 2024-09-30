5b8f9b6296f2f79c1c7f7a22581b081d20df0cfa
Anthony Tran <atran5591@gmail.com>
2024-09-29 21:26:13 -0700

    update route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,26 +4,26 @@ export const routes: Routes = [
   {
     path: 'login',
     loadComponent: () =>
-      import('./features/login/login.component').then((m) => m.LoginComponent),
+      import('./user-flow/login/login.component').then((m) => m.LoginComponent),
   },
   {
     path: 'sign-up',
     loadComponent: () =>
-      import('./features/sign-up/sign-up.component').then(
+      import('./user-flow/sign-up/sign-up.component').then(
         (m) => m.SignUpComponent
       ),
   },
   {
     path: 'reset-password',
     loadComponent: () =>
-      import('./features/reset-password/reset-password.component').then(
+      import('./user-flow/reset-password/reset-password.component').then(
         (m) => m.ResetPasswordComponent
       ),
   },
   {
     path: 'forgot-password',
     loadComponent: () =>
-      import('./features/forgot-password/forgot-password.component').then(
+      import('./user-flow/forgot-password/forgot-password.component').then(
         (m) => m.ForgotPasswordComponent
       ),
   },
