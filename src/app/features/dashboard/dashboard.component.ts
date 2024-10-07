672c03535f03e51b19932ebf6281fbb659640685
Anthony Tran <atran5591@gmail.com>
2024-10-06 23:00:12 -0700

    more generic templates

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,43 +1,14 @@
 import { CommonModule } from '@angular/common';
-import {
-  ChangeDetectionStrategy,
-  Component,
-  inject,
-  signal,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
-import { combineLatest, filter, map } from 'rxjs';
-import { EveryoneVotesService } from '../../services/everyone-votes/everyone-votes.service';
-import { PollComponent } from '../poll/poll.component';
+
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, CardModule, ButtonModule, PollComponent],
+  imports: [CommonModule, CardModule, ButtonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DashboardComponent {
-  everyoneVotesService = inject(EveryoneVotesService);
-  processing = signal(false);
-
-  polls$ = this.everyoneVotesService
-    .getPolls()
-    .pipe(map(({ data, error }) => data));
-
-  userVotes$ = this.everyoneVotesService
-    .getUserVotes()
-    .pipe(map(({ data, error }) => data));
-
-  pollsAndVotes$ = combineLatest([this.polls$, this.userVotes$]).pipe(
-    filter(([polls]) => polls !== null),
-    map(([polls, votes]) => {
-      if (!votes) return polls;
-      return polls?.map((poll) => {
-        const userVote = votes?.find((vote) => vote.poll_id === poll.id);
-        return { ...poll, userVote };
-      });
-    })
-  );
-}
+export class DashboardComponent {}
